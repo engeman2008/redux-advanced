a90a77fe992150c846f059b62ec1a791b10cabf6
Eman <eman.cse2008@gmail.com>
2020-09-25 15:42:29 +0200

    redux devtools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -9,12 +10,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action)
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState())
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
